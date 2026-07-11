0d8e47474a3e476a8a97c3de8ce193433b410d95
Paras Chaudhary <paraschaudhary.0125@gmail.com>
2026-07-11 18:48:29 +0530

    Time: 53 ms (91.48%), Space: 105.6 MB (48.6%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
@@ -3,9 +3,13 @@ class Solution {
         Set<Integer> s=new HashSet<>();
         for(int n:nums){
             s.add(n);
-            StringBuilder k=new StringBuilder(String.valueOf(n));
-            k.reverse();
-            s.add(Integer.parseInt(k.toString()));
+            int rev=0;
+            int temp=n;
+            while(temp>0){
+                rev=rev*10+temp%10;
+                temp/=10;
+            }
+            s.add(rev);
         }
         return s.size();
     }

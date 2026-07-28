886f8ac33d141319b2388476027e45852c7afe4f
Paras Chaudhary <paraschaudhary.0125@gmail.com>
2026-07-28 23:42:55 +0530

    Time: 4 ms (19.57%), Space: 43.8 MB (17.92%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        String s="";
+        int n=word1.length();
+        int m=word2.length();
+        int i=0;
+        int j=0;
+        while(i<n && j<m){
+            s+=word1.charAt(i++);
+            s+=word2.charAt(j++);
+        }
+        while(i<n){
+            s+=word1.charAt(i++);
+        }
+        while(j<m){
+            s+=word2.charAt(j++);
+        }
+        return s;
+    }
+}

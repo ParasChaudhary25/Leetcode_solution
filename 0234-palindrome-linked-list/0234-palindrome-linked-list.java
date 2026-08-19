7a4510c3101a8101ad4ec67efadedcb7f2609e3b
Paras Chaudhary <paraschaudhary.0125@gmail.com>
2026-08-19 15:13:53 +0530

    Time: 3 ms (99.82%), Space: 93.9 MB (96.14%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,39 +9,32 @@
  * }
  */
 class Solution {
-    public ListNode findMid(ListNode head){
+    public boolean isPalindrome(ListNode head) {
+        if(head==null || head.next==null){
+            return true;
+        }
         ListNode end=head;
         ListNode mid=head;
         while(end.next!=null && end.next.next!=null){
             end=end.next.next;
             mid=mid.next;
         }
-        return mid;
-    }
-    public ListNode reverse(ListNode s){
+        
         ListNode prev=null;
-        ListNode cur=s;
+        ListNode cur=mid.next;
         while(cur!=null){
             ListNode n=cur.next;
-            
             cur.next=prev;
             prev=cur;
             cur=n;
         }
-        return prev;
-    }
-    public boolean isPalindrome(ListNode head) {
-        if(head==null || head.next==null){
-            return true;
-        }
-        ListNode mid=findMid(head);
-        ListNode second=reverse(mid.next);
+        
         ListNode first=head;
-        while(second!=null){
-            if(first.val!=second.val){
+        while(prev!=null){
+            if(first.val!=prev.val){
                 return false;
             }
-            second=second.next;
+            prev=prev.next;
             first=first.next;
         }
         return true;
